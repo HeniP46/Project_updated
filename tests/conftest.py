0a2e0e9c6dc13f45bcc0819ba0f7c74c5bbf86ff
hpp46 <hpp46@njit.edu>
2025-06-30 18:18:26 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,15 +17,15 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
-
+        
         if operation_func == divide and b == 0:
             b = Decimal('1')
-
+        
         try:
             expected = operation_func(a, b)
         except ZeroDivisionError:
             expected = "ZeroDivisionError"
-
+        
         yield a, b, operation_name, operation_func, expected
 
 def pytest_addoption(parser):
