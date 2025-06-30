d41de4c30a29b6be4b1fc29b010c9d2cf873bab3
hpp46 <hpp46@njit.edu>
2025-06-30 18:48:15 -0400

    Update test_extra_coverage.py

--- tests/test_extra_coverage.py
+++ tests/test_extra_coverage.py
@@ -1,10 +1,10 @@
 import pytest
-from app import App
 from app.commands.add import AddCommand
 from app.commands.subtract import SubtractCommand
 from app.commands.multiply import MultiplyCommand
 from app.commands.divide import DivideCommand
 from app import plugin_loader
+from app import App
 
 
 def test_app_empty_input_then_exit(monkeypatch, capfd):
@@ -26,7 +26,7 @@ def test_command_names():
 def test_divide_by_zero():
     cmd = DivideCommand()
     with pytest.raises(ZeroDivisionError):
-        cmd.execute("5", "0")
+        cmd.execute("10", "0")
 
 
 def test_load_commands_returns_dict():
