597092e87a84433c2b993ca4c0d1091a2c5defb2
hpp46 <hpp46@njit.edu>
2025-06-30 18:16:00 -0400

    Update add.py

--- app/commands/add.py
+++ app/commands/add.py
@@ -1,11 +1,10 @@
-from decimal import Decimal
 from app.operations import add
+from decimal import Decimal
 
 class AddCommand:
-    def __init__(self, value1: Decimal, value2: Decimal):
-        self.value1 = value1
-        self.value2 = value2
-
+    def __init__(self, a: Decimal, b: Decimal):
+        self.a = a
+        self.b = b
+    
     def execute(self):
-        return add(self.value1, self.value2)
-
+        return add(self.a, self.b)
