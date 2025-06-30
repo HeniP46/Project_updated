8fe6e4196eef8817b82cd269cae627499da10ac0
hpp46 <hpp46@njit.edu>
2025-06-30 18:28:47 -0400

    Update calculation.py

--- app/calculation.py
+++ app/calculation.py
@@ -1,14 +1,19 @@
 from decimal import Decimal
 from typing import Callable
+
+
 class Calculation:
-    def init(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
         self.a = a
         self.b = b
         self.operation = operation
+
     @staticmethod
     def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
         return Calculation(a, b, operation)
+
     def perform(self) -> Decimal:
         return self.operation(self.a, self.b)
-    def repr(self):
-        return f"Calculation({self.a}, {self.b}, {self.operation.name})"
+
+    def __repr__(self):
+        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
