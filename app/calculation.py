ca52c2cee9626a60a30fbc44379af984442528e2
hpp46 <hpp46@njit.edu>
2025-06-30 18:25:36 -0400

    Update calculation.py

--- app/calculation.py
+++ app/calculation.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from typing import Callable
 
+
 class Calculation:
     def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
         self.a = a
